ffa7db6d9d29cea721629a25c85283e248be4623
Junwon Lee <cpprhtn@naver.com>
2022-08-16 22:42:48 +0900

    feat: featapi skeleton code

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+import model
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/food_rs")
+def food_rs():
+    with session_factory() as session:
+        #user history api 받아오기 + 전처리
+        #pred 함수 call
+        return pred
+    
+@app.get("/category_rs")
+def category_rs():
+    with session_factory() as session:
+        #user history api 받아오기 + 전처리
+        #pred 함수 call
+        return pred
